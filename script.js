41f26fecb12d0a952dd97c680a164e266671502e
Dementexo <browndon962@gmail.com>
2022-06-08 23:25:48 -0500

    Added foundations for rps game

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log("Welcome to Rock Paper Scissors! Please input your choice.");
+let computerScore = 0;
+let playerScore = 0;
+let tempPlayerInput;
 
-const playerSelection = prompt();
-
+game();
 
 function computerPlay(mainChoice){
     let decider=Math.floor(Math.random()*10);
@@ -13,25 +14,28 @@ function computerPlay(mainChoice){
         mainChoice = "paper";
         return mainChoice;
     }
-    else if (decider >6 && decider <=9){
+    else if (decider >= 6 && decider <= 9){
         mainChoice = "scissors";
         return mainChoice;
     }
 }
 
-const computerSelection = computerPlay();
-
 function playRound(){
+    let playerSelection = prompt();
+    let computerSelection = computerPlay();
     let playerTurn=playerSelection.toLowerCase();
+    tempPlayerInput = playerTurn;
     if(playerTurn == "rock"){
         if(computerSelection == "rock"){
             console.log("You both chose rock, tie!");
         }
         else if(computerSelection == "paper"){
             console.log("You chose rock and the computer chose paper, you lose!");
+            ++computerScore;
         }
         else if(computerSelection == "scissors"){
             console.log("You chose rock and the computer chose scissors, you win!");
+            ++playerScore;
         }
     }
     if(playerTurn == "paper"){
@@ -40,9 +44,11 @@ function playRound(){
         }
         else if(computerSelection == "scissors"){
             console.log("You chose paper while the computer chose scissors, you lose!");
+            ++computerScore;
         }
         else if(computerSelection == "rock"){
             console.log("You chose paper while the computer chose rock, you win!");
+            ++playerScore;
         }
     }
     if(playerTurn == "scissors"){
@@ -51,11 +57,29 @@ function playRound(){
         }
         else if(computerSelection == "rock"){
             console.log("You chose scissors while the computer chose rock, you lose!");
+            ++computerScore;
         }
         else if(computerSelection == "paper"){
             console.log("You chose scissors while the computer chose paper, you win!");
+            ++playerScore;
         }
     }
 }
 
-playRound();
+function game(){
+    console.log("Welcome to rock, paper, scissors! You will be fighting against the computer.")
+    for (i=0; i=5;){
+        playRound();
+        if (computerPlay() != tempPlayerInput){
+            ++i;
+        }
+        if (computerScore >= 3){
+            console.log("The computer wins! You lose HAHAHAHAHA!")
+            break;
+        }
+        else if (playerScore >= 3){
+            console.log("You win, that's cool... I guess.")
+            break;
+        }
+    }
+}
