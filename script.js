c0f2b7df50aa7b008d4de8b85064c9dc3e2806b7
Dementexo <browndon962@gmail.com>
2022-06-11 23:18:02 -0500

    Added navBar and updated image on repository.

--- script.js
+++ script.js
@@ -42,39 +42,48 @@ function playRound(){
         if(playerSelection == "rock"){
             if(computerSelection == "rock"){
                 console.log("You both chose rock, tie!");
+                document.getElementById("announcementBox").innerHTML = "You both chose rock, tie!";
             }
             else if(computerSelection == "paper"){
                 console.log("You chose rock and the computer chose paper, you lose!")
+                document.getElementById("announcementBox").innerHTML = "You chose rock and the computer chose paper, you lose!";
                 ++computerScore;
             }
             else if(computerSelection == "scissors"){
                 console.log("You chose rock and the computer chose scissors, you win!");
+                document.getElementById("announcementBox").innerHTML = "You chose rock and the computer chose scissors, you win!";
                 ++playerScore;
             }
         }
         if(playerSelection == "paper"){
             if(computerSelection == "paper"){
                 console.log("You both chose paper, tie!");
+                document.getElementById("announcementBox").innerHTML = "You both chose paper, tie!";
             }
             else if(computerSelection == "scissors"){
                 console.log("You chose paper while the computer chose scissors, you lose!");
+                document.getElementById("announcementBox").innerHTML = "You chose paper while the computer chose scissors, you lose!";
                 ++computerScore;
             }
             else if(computerSelection == "rock"){
                 console.log("You chose paper while the computer chose rock, you win!");
+                document.getElementById("announcementBox").innerHTML = "You chose paper while the computer chose rock, you win!";
                 ++playerScore;
             }
         }
         if(playerSelection == "scissors"){
             if(computerSelection == "scissors"){
                 console.log("You both chose scissors, tie!");
+                document.getElementById("announcementBox").innerHTML = "You both chose scissors, tie!";
             }
             else if(computerSelection == "rock"){
                 console.log("You chose scissors while the computer chose rock, you lose!");
+                document.getElementById("announcementBox").innerHTML = "You chose scissors while the computer chose rock, you lose!";
                 ++computerScore;
             }
             else if(computerSelection == "paper"){
                 console.log("You chose scissors while the computer chose paper, you win!");
+                document.getElementById("announcementBox").innerHTML = "You chose scissors while the computer chose paper, you win!";
                 ++playerScore;
             }
         }
@@ -84,14 +93,20 @@ function playRound(){
     if (playerScore == 3 || computerScore == 3){
         if (playerScore == 3){
             console.log("Player wins!")
+            document.getElementById("announcementBox").innerHTML = "You won! Congrats!";
         }
         else if (computerScore == 3){
             console.log("Computer wins!")
+            document.getElementById("announcementBox").innerHTML = "You lost! Better luck next time";
         }
     }
 }
 function gameStart(){
     console.log("Welcome to Rock, Paper, Scissors! Please pick a choice for your first turn.");
+    document.getElementById("announcementBox").innerHTML = "Let the game begin!";
+    document.getElementById("announcementBox").style.visibility = "visible";
+    document.getElementById("playerScore").innerHTML = 0;
+    document.getElementById("computerScore").innerHTML = 0;
     playerScore = 0;
     computerScore = 0;
     choiceRock.addEventListener('click', rockSelect_Play);
